fb46f8f10dc0f0c6664e8f9a4af44a17316cbac0
Mark Leo <ratrateroo@gmail.com>
2019-12-21 01:46:44 +0800

    fetched products with mongoose

--- mongoose.js
+++ mongoose.js
@@ -16,8 +16,14 @@ const createProduct = async (req, res, next) => {
     price: req.body.price
   });
   const result = await createdProduct.save();
-
+  console.log(typeof createProduct._id);
   res.json(result);
 };
 
+const getProducts = async (req, res, next) => {
+  const products = await Product.find().exec();
+  res.json(products);
+}
+
 exports.createProduct = createProduct;
+exports.getProducts = getProducts;
